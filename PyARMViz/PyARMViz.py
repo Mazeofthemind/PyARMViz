39239044b14b2dc5eaf0e4ed170cde0011a08873
nachigonz <nachi.g2001@gmail.com>
2021-06-14 16:55:15 -0500

    Update PyARMViz.py

--- PyARMViz/PyARMViz.py
+++ PyARMViz/PyARMViz.py
@@ -358,11 +358,6 @@ def adjacency_graph_plotly(rules:Rule):
             showlegend=False,
             hovermode='closest',
             margin=dict(b=20,l=5,r=5,t=40),
-            annotations=[ dict(
-                text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
-                showarrow=False,
-                xref="paper", yref="paper",
-                x=0.005, y=-0.002 ) ],
             xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
             yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
             )
