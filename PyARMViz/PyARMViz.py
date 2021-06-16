ff6fcaa8d0d74f5628d167157ef6c716842044be
nachigonz <nachi.g2001@gmail.com>
2021-06-16 16:56:22 -0500

    Update PyARMViz.py

--- PyARMViz/PyARMViz.py
+++ PyARMViz/PyARMViz.py
@@ -347,7 +347,7 @@ def adjacency_graph_plotly(rules:Rule):
 
     node_trace = go.Scatter(
         x=node_x, y=node_y,
-        mode='markers',
+        mode='text',
         #hoverinfo='text',
         text=node_text,
         marker=dict(
